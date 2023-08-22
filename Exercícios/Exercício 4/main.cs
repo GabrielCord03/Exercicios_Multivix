51102a3ca54d238c72a04591a79869a7143b93d9
GabrielCord03 <141854199+GabrielCord03@users.noreply.github.com>
2023-08-22 17:35:11 -0300

    Update main.cs

--- Exercícios/Exercício 4/main.cs
+++ Exercícios/Exercício 4/main.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+    // este comando for imprime do 0 até o 20 pulando de dois em dois
     for (int i = 0; i <= 20; i += 2){
       Console.WriteLine(i);
     }
